221d48157f67ac93dff7ab00a99c9530463daf62
Novichenko Vitaliy <novichenkovitaliy@gmail.com>
2023-08-15 00:46:55 +0300

    Initial Commit

---
+++ iConverter/iConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConverterApp.swift
+//  iConverter
+//
+//  Created by Vitaliy Novichenko on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
